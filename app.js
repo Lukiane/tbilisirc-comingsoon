5e1cbba6ff90e584523fedbe346a3e42bf08c9eb
Luka Kobalia <lkobal2013@gmail.com>
2018-07-18 14:38:25 +0400

    Finla Comming Soon Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+//app.use('/Public', express.static('/Public'));
+// GET /static/style.css etc.
+app.use(express.static('assets'));
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/assets/index.html');
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => console.log('Server Started - Listening 3000'));
